7107d30852656c5dd96b8c4ab4f5c0f040237074
Etzernal <chan.jielin@dhs.sg>
2020-04-18 01:29:27 +0800

    Adds shell functionality to CP2

--- PA2/ClientCP2.java
+++ PA2/ClientCP2.java
@@ -2,48 +2,60 @@ import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
+import javax.crypto.Cipher;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Arrays;
 import java.security.*;
 import javax.crypto.*;
-import java.util.Base64;
+import java.lang.*; 
+import java.io.*; 
+import java.util.*;
 
 public class ClientCP2 {
 
-	public static void main(String[] args) {
+	public static DataOutputStream toServer = null;
+    public static DataInputStream fromServer = null;
 
-    	String filename = "100.txt";
-    	if (args.length > 0) filename = args[0];
+	public static FileInputStream fileInputStream = null;
+    public static BufferedInputStream bufferedFileInputStream = null;
+    public static Socket clientSocket = null;
 
-    	String serverAddress = "localhost";
-    	if (args.length > 1) filename = args[1];
+	public static long timeStarted = System.nanoTime();
 
-    	int port = 4321;
-    	if (args.length > 2) port = Integer.parseInt(args[2]);
+	public static int numBytes = 0;
 
-		int numBytes = 0;
+	public static void shellListDir(){
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.command("ls");
+		try {
+			Process p = processBuilder.start(); 
+			StringBuilder output = new StringBuilder(); 
+	     
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        String line;
+			while ((line = reader.readLine()) != null) {
+			 	output.append(line + "\n");
+			}
+	        int exitVal = p.waitFor();
+	       	if (exitVal == 0) {
+				System.out.println("Success!");
+				System.out.println(output);
+		    } else {
+			    //abnormal...
+			    System.out.println("Error has occurred while listing directory, please try again");
+		    }
+		} catch (Exception e){
+			System.out.println("Error found in DeleteFile");
+			e.printStackTrace();
+		}
+	}
 
-		Socket clientSocket = null;
-
-        DataOutputStream toServer = null;
-        DataInputStream fromServer = null;
-
-    	FileInputStream fileInputStream = null;
-        BufferedInputStream bufferedFileInputStream = null;
-
-		long timeStarted = System.nanoTime();
+	public static void shellUploadFile(String filename){
+		System.out.println("Sending file...");
 
 		try {
-
-			System.out.println("Establishing connection to server...");
-
-			// Connect to server and get the input and output streams
-			clientSocket = new Socket(serverAddress, port);
-			toServer = new DataOutputStream(clientSocket.getOutputStream());
-			fromServer = new DataInputStream(clientSocket.getInputStream());
-
-			System.out.println("Sending file...");
-
 			// Send the filename
 			toServer.writeInt(0);
 			toServer.writeInt(filename.getBytes().length);
@@ -51,8 +63,8 @@ public class ClientCP2 {
 			toServer.flush();
 
 			// Generate AES encryption key
-        	KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        	SecretKey aesKey = keyGen.generateKey();
+	    	KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+	    	SecretKey aesKey = keyGen.generateKey();
 
 			// Send Encrypted AES key
 
@@ -82,7 +94,7 @@ public class ClientCP2 {
 	        // PublicKey key = AuthenticationProtocol.getPubKey("../cacse.crt", "../server.crt");
 	        if (key != null) {
 	        	Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        		aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+	    		aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
 
 			    // Send the file
 			    for (boolean fileEnded = false; !fileEnded;) {
@@ -101,6 +113,119 @@ public class ClientCP2 {
 					toServer.flush();
 				}
 	        }
+	        System.out.println("Closing connection...");
+			long timeTaken = System.nanoTime() - timeStarted;
+			System.out.println("Program took: " + timeTaken/1000000.0 + "ms to run");
+	    } catch (Exception e){
+	    	e.printStackTrace();
+	    }
+	}
+	// public static void shellDownloadFile(){ //Stupid to have download :D wake up your idea pls.
+
+	// }
+	public static void shellDeleteFile(String filename){
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.command("rm",filename);
+		try {
+			Process p = processBuilder.start(); 
+			StringBuilder output = new StringBuilder(); 
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        String line;
+			while ((line = reader.readLine()) != null) {
+			 	output.append(line + "\n");
+			}
+	        int exitVal = p.waitFor();
+	       	if (exitVal == 0) {
+				System.out.println("Success!");
+				System.out.println(output);
+		    } else {
+			    //abnormal...
+			    System.out.println("File cannot be deleted, please try again");
+		    }
+		} catch (Exception e){
+			System.out.println("Error found in DeleteFile");
+			e.printStackTrace();
+		}
+	}
+
+	public static int shellExecuteInput(String line){
+		String[] words = line.split("\\s");
+		if (words.length > 2){
+			System.out.println("Invalid command, please try: EXIT, UPLD <FILENAME>, DWNLD <FILENAME>, DEL <FILENAME> or LISTDIR");
+			return 1;
+		}
+		//:D //LISTDIR 
+		else if (line.compareTo("LISTDIR") == 0){
+			System.out.println("Listing directory");
+			shellListDir();
+			return 1;
+		}
+		//:D //EXIT -program took how long to run, method used was aes1
+		else if (line.compareTo("EXIT") == 0){
+			//kill
+			System.out.println("Exiting now");
+			System.exit(0);
+		}
+		//UPLD (filename)
+		else if (words[0].compareTo("UPLD") == 0){
+			System.out.println("Uploading mode");
+			shellUploadFile(words[1]);
+			return 1; 
+		}
+		//DWNLD (filename)
+		else if (words[0].compareTo("DWNLD") == 0){
+			System.out.println("Downloading mode");
+			// shellDownloadFile();
+			return 1;
+		}
+		//:D DEL (filename) DEL non-existant file
+		else if (words[0].compareTo("DEL") == 0){
+			System.out.println("Deleting mode");
+			shellDeleteFile(words[1]);
+			return 1; 
+		}
+		else {
+			//invalid commands
+			System.out.println("Invalid command, please try: EXIT, UPLD <FILENAME>, DWNLD <FILENAME>, DEL <FILENAME> or LISTDIR");
+			return 1; 
+		}
+		return 1;
+	}
+
+	public static void shellLoop(){
+		String args;
+		int status;
+		Scanner sc= new Scanner(System.in);
+		System.out.print(">>CSESHELL>> ");
+		String line= sc.nextLine();
+		status = shellExecuteInput(line); 
+		if (status == 1){
+			shellLoop();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Shell has started...");
+    	String filename = "100.txt";
+    	if (args.length > 0) filename = args[0];
+
+    	String serverAddress = "localhost";
+    	if (args.length > 1) filename = args[1];
+
+    	int port = 4321;
+    	if (args.length > 2) port = Integer.parseInt(args[2]);
+
+		try {
+
+			System.out.println("Establishing connection to server...");
+
+			// Connect to server and get the input and output streams
+			clientSocket = new Socket(serverAddress, port);
+			toServer = new DataOutputStream(clientSocket.getOutputStream());
+			fromServer = new DataInputStream(clientSocket.getInputStream());
+
+			shellLoop();
 	        
 
 	        bufferedFileInputStream.close();
